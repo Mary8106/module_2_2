ba82797ca4efc37e444606899ea4a05ec4fc20e9
Mary8106 <2705396@mail.ru>
2024-07-22 22:03:43 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,19 @@
+#first = 243
+#second = 674
+#third = 95
+#if first==second and second==third:
+#    print(3)
+#elif first==second or first==third or second==third:
+#    print(2)
+#elif first!=second and first!=third and second !=third:
+#    print(0)
+
+first = 42
+second = 69
+third = 42
+if first==second and second==third:
+    print(3)
+elif first==second or first==third or second==third:
+    print(2)
+elif first!=second and first!=third and second !=third:
+    print(0)
